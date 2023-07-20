59a48d299b7988fde1251f1748b672603604f778
Erich Malberg <emalberg@live.com>
2023-07-19 18:01:27 -0600

    Starting mobile app basics

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import * as React from 'react';
+import { PaperProvider } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open Erich to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
